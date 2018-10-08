2498c9ed2f1df0471d899d30017e29046b380f67
AlexinaO <alexina.orsoni@gmail.com>
2018-10-08 12:10:59 +0200

    Tests pour la validation des lignes : positiveQuantity + InfoProduct

--- ConsolePourTestTests/CartLineTest.cs
+++ ConsolePourTestTests/CartLineTest.cs
@@ -1,5 +1,6 @@
-﻿using System;
+﻿using ConsolePourTest.Entities;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
 
 namespace ConsolePourTestTests
 {
@@ -7,8 +8,27 @@ namespace ConsolePourTestTests
     public class CartLineTest
     {
         [TestMethod]
-        public void TestMethod1()
+        public void ValidatePositiveQuantity()
         {
+            var cartLine = new CartLine
+            {
+                Product = new Product(),
+                Quantity = -1
+            };
+            var exception = Assert.ThrowsException<Exception>(() => cartLine.Validate());
+            Assert.AreEqual("Quantité doit être positive", exception.Message);
+        }
+
+        [TestMethod]
+        public void ValidateInfoProduct()
+        {
+            var cartLine = new CartLine
+            {
+                Product = null,
+                Quantity = 1
+            };
+            var exception = Assert.ThrowsException<Exception>(() => cartLine.Validate());
+            Assert.AreEqual("Produit est requis", exception.Message);
         }
     }
 }
