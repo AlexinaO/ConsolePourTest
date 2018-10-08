ac565c2c03167d474c6982344e4bfc8bc204b7b4
AlexinaO <alexina.orsoni@gmail.com>
2018-10-08 10:39:15 +0200

    Création de la classe Cart avec ses attributs

---
+++ ConsolePourTest/Entities/Cart.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace ConsolePourTest.Entities
+{
+    public class Cart
+    {
+        public Cart()
+        {
+            Lines = new List<CartLine>();
+        }
+
+        public List<CartLine> Lines { get; set; }
+
+        public decimal ShippingCost { get; set; } = 10;
+
+        public decimal GetTotal()
+        {
+            var totalLines = Lines.Sum(x => x.Product.Price * x.Quantity);
+            return totalLines + ShippingCost;
+        }
+    }
+}
