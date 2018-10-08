12ed7fc90cc8a9d0501ddeaa73ba839893741442
AlexinaO <alexina.orsoni@gmail.com>
2018-10-08 10:22:37 +0200

    Initialisation du projet

---
+++ ConsolePourTestTests/UnitTest1.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace ConsolePourTestTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            int i = 1;
+            int j = 2;
+            Assert.AreEqual(3, i + j); //test vrai si i+j = 3
+        }
+
+        [TestMethod]
+        public void TestMethodAge()
+        {
+            var dateNaissance = new DateTime(2010, 5, 1);
+            Assert.IsTrue(DateTime.Today.Subtract(dateNaissance).Days / 365 > 10); //test pour savoir si âge >10 ans
+        }
+    }
+}
