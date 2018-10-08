f8fc654f3fc10114fb1bfe2c8128a8c2cdca03c7
AlexinaO <alexina.orsoni@gmail.com>
2018-10-08 10:41:03 +0200

    Création du test CartTest avec les méthodes

---
+++ ConsolePourTestTests/CartTest.cs
@@ -0,0 +1,32 @@
+﻿using ConsolePourTest.Entities;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ConsolePourTestTests
+{
+    [TestClass]
+    public class CartTest
+    {
+        [TestMethod]
+        public void TotalPriceCalculationWhenEmpty()
+        {
+            var cart = new Cart();
+            Assert.AreEqual(0, cart.GetTotal());
+        }
+
+        [TestMethod]
+        public void TotalPriceCalculationNormal()
+        {
+            var cart = new Cart();
+            cart.Lines.Add(new CartLine
+            {
+                Product = new Product
+                {
+                    Name = "Tronçonneuse",
+                    Price = 90
+                },
+                Quantity = 1
+            });
+            Assert.AreEqual(100, cart.GetTotal());
+        }
+    }
+}
