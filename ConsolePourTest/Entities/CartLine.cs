19ac2aac0c83078bb881acdb8982d901bf3dc551
AlexinaO <alexina.orsoni@gmail.com>
2018-10-08 10:39:37 +0200

    Création de la classe CartLine avec ses attributs

---
+++ ConsolePourTest/Entities/CartLine.cs
@@ -0,0 +1,11 @@
+﻿namespace ConsolePourTest.Entities
+{
+    public class CartLine
+    {
+        //public int Id { get; set; } pas utile si on ne met pas en base !
+
+        public Product Product { get; set; }
+
+        public int Quantity { get; set; }
+    }
+}
