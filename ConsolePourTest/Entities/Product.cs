f8534f8d570eba27cd9c010a77994c99773c8c00
AlexinaO <alexina.orsoni@gmail.com>
2018-10-08 11:02:46 +0200

    Ajout d'une méthode Validate dans la classe Product

--- ConsolePourTest/Entities/Product.cs
+++ ConsolePourTest/Entities/Product.cs
@@ -1,11 +1,32 @@
-﻿namespace ConsolePourTest.Entities
+﻿using System;
+
+namespace ConsolePourTest.Entities
 {
     public class Product
     {
+        private readonly decimal price;
+
         public int Id { get; set; }
 
         public string Name { get; set; }
 
-        public decimal Price { get; set; }
+        public decimal Price
+        {
+            get { return this.price; }
+            set
+            {
+                if (value <= 0)
+                    throw new Exception("Prix ne peut pas être négatif");
+                Price = value;
+            }
+        }
+
+        public void Validate()
+        {
+            if (Price <= 0)
+                throw new Exception("Prix ne peut pas être négatif");
+            if (string.IsNullOrEmpty(Name))
+                throw new Exception("Nom est requis");
+        }
     }
 }
