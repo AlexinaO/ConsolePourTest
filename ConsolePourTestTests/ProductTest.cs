3865dd25b469dc41297ef09a04d8f3a74a307c51
AlexinaO <alexina.orsoni@gmail.com>
2018-10-08 11:03:31 +0200

    Ajout des tests pour Prix qui ne doit pas être <=0 et name vide

---
+++ ConsolePourTestTests/ProductTest.cs
@@ -0,0 +1,38 @@
+﻿using ConsolePourTest.Entities;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace ConsolePourTestTests
+{
+    [TestClass]
+    public class ProductTest
+    {
+        [TestMethod]
+        public void NoPriceInfOrEqual0()
+        {
+            var exception = Assert.ThrowsException<Exception>(() =>
+            {
+                var product = new Product
+                {
+                    Name = "Bouteille Bordeaux",
+                    Price = 0
+                };
+            });
+            Assert.AreEqual("Prix ne peut pas être négatif", exception.Message);
+        }
+
+        [TestMethod]
+        public void NoNameEmpty()
+        {
+            Assert.ThrowsException<Exception>(() =>
+            {
+                var product = new Product
+                {
+                    Name = "",
+                    Price = 10
+                };
+                product.Validate();
+            });
+        }
+    }
+}
